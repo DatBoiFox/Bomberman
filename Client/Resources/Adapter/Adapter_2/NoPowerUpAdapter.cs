3f593b0e0547df80e42c992b7a8e829297970438
Kyo <deividaspt@gmail.com>
2019-12-09 13:33:19 +0200

    Null Object

---
+++ Client/Resources/Adapter/Adapter_2/NoPowerUpAdapter.cs
@@ -0,0 +1,30 @@
+﻿using Client.Resources.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client.Resources.Adapter.Adapter_2
+{
+    class NoPowerUpAdapter : IPowerUp
+    {
+        public int id { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+        public PictureBox model { get; set; }
+
+        public PowerUp powerUp;
+
+        public NoPowerUpAdapter()
+        {
+            this.powerUp = null;
+        }
+
+        public string getPowerUpType()
+        {
+            return "No Power Up";
+        }
+    }
+}
---
+++ Client/Resources/Builder/Template/EmptyCrateBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Resources.Adapter.Adapter_2;
+
+namespace Client.Resources.Builder.Template
+{
+    class EmptyCrateBuilder : PowerUpCrateBuilder
+    {
+        public override void AddPowerUp()
+        {
+            crate.powerUp = new NoPowerUpAdapter();
+        }
+
+        public override void SetColor()
+        {
+            crate.color = null;
+        }
+    }
+}
